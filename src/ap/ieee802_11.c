d4f8c93a405baae5f70b095361860ee69b1f2bef
Thomas Pedersen <thomas@cozybit.com>
2014-03-10 20:11:14 -0700

    auth: handle auth frames for mesh ifaces
    Modify wpa_supplicant/hostapd logic to allow SAE auth on Mesh
    interfaces.
    
    Signed-off-by: Javier Lopez <jlopex@cozybit.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -457,6 +457,18 @@ static void handle_auth_sae(struct hostapd_data *hapd, struct sta_info *sta,
 		sta->sae->state = SAE_NOTHING;
 	}
 
+#ifdef CONFIG_MESH
+	if (sta->sae->state == SAE_ACCEPTED) {
+		if (auth_transaction == 1 || auth_transaction == 2) {
+			wpa_printf(MSG_DEBUG, "SAE: remove the STA "
+				   "(" MACSTR ") doing reauthentication",
+				   MAC2STR(sta->addr));
+			ap_free_sta(hapd, sta);
+		}
+		return;
+	}
+#endif /* CONFIG_MESH */
+
 	if (auth_transaction == 1) {
 		const u8 *token = NULL;
 		size_t token_len = 0;
@@ -482,16 +494,29 @@ static void handle_auth_sae(struct hostapd_data *hapd, struct sta_info *sta,
 					   MAC2STR(sta->addr));
 				data = auth_build_token_req(hapd, sta->addr);
 				resp = WLAN_STATUS_ANTI_CLOGGING_TOKEN_REQ;
-			} else {
+			} else if (sta->sae->state != SAE_COMMITTED) {
 				data = auth_process_sae_commit(hapd, sta);
 				if (data == NULL)
 					resp = WLAN_STATUS_UNSPECIFIED_FAILURE;
 				else
 					sta->sae->state = SAE_COMMITTED;
+			} else if (sae_process_commit(sta->sae) < 0) {
+					wpa_printf(MSG_DEBUG,
+					 "SAE: Failed to process peer commit");
+					resp = WLAN_STATUS_UNSPECIFIED_FAILURE;
+			} else {
+				data = auth_build_sae_confirm(hapd, sta);
+				if (data == NULL)
+					resp = WLAN_STATUS_UNSPECIFIED_FAILURE;
+				else {
+					sta->sae->state = SAE_CONFIRMED;
+					auth_transaction = 2;
+				}
 			}
 		}
 	} else if (auth_transaction == 2) {
-		if (sta->sae->state != SAE_COMMITTED) {
+		if (sta->sae->state != SAE_COMMITTED &&
+		    sta->sae->state != SAE_CONFIRMED) {
 			hostapd_logger(hapd, sta->addr,
 				       HOSTAPD_MODULE_IEEE80211,
 				       HOSTAPD_LEVEL_DEBUG,
@@ -512,6 +537,13 @@ static void handle_auth_sae(struct hostapd_data *hapd, struct sta_info *sta,
 			wpa_auth_sm_event(sta->wpa_sm, WPA_AUTH);
 			sta->auth_alg = WLAN_AUTH_SAE;
 			mlme_authenticate_indication(hapd, sta);
+
+			/* already confirmed */
+			if (sta->sae->state == SAE_CONFIRMED) {
+				sta->sae->state = SAE_ACCEPTED;
+				sae_clear_temp_data(sta->sae);
+				return;
+			}
 
 			data = auth_build_sae_confirm(hapd, sta);
 			if (data == NULL)
@@ -650,10 +682,20 @@ static void handle_auth(struct hostapd_data *hapd,
 		return;
 	}
 
-	sta = ap_sta_add(hapd, mgmt->sa);
-	if (!sta) {
-		resp = WLAN_STATUS_AP_UNABLE_TO_HANDLE_NEW_STA;
-		goto fail;
+#ifdef CONFIG_MESH
+	if (hapd->conf->mesh & MESH_ENABLED) {
+		/* if the mesh peer is not available, we don't do authentication. */
+		sta = ap_get_sta(hapd, mgmt->sa);
+		if (!sta)
+			return;
+	} else
+#endif /* CONFIG_MESH */
+	{
+		sta = ap_sta_add(hapd, mgmt->sa);
+		if (!sta) {
+			resp = WLAN_STATUS_AP_UNABLE_TO_HANDLE_NEW_STA;
+			goto fail;
+		}
 	}
 
 	if (vlan_id > 0) {
@@ -738,6 +780,19 @@ static void handle_auth(struct hostapd_data *hapd,
 #endif /* CONFIG_IEEE80211R */
 #ifdef CONFIG_SAE
 	case WLAN_AUTH_SAE:
+#ifdef CONFIG_MESH
+		if (hapd->conf->mesh & MESH_ENABLED) {
+			if (sta->wpa_sm == NULL)
+				sta->wpa_sm = wpa_auth_sta_init(hapd->wpa_auth,
+								sta->addr, NULL);
+			if (sta->wpa_sm == NULL) {
+				wpa_printf(MSG_DEBUG, "FT: Failed to initialize WPA "
+					   "state machine");
+				resp = WLAN_STATUS_UNSPECIFIED_FAILURE;
+				goto fail;
+			}
+		}
+#endif /* CONFIG_MESH */
 		handle_auth_sae(hapd, sta, mgmt, len, auth_transaction);
 		return;
 #endif /* CONFIG_SAE */
@@ -1760,6 +1815,9 @@ int ieee802_11_mgmt(struct hostapd_data *hapd, const u8 *buf, size_t len,
 	    !((hapd->conf->p2p & P2P_GROUP_OWNER) &&
 	      stype == WLAN_FC_STYPE_ACTION) &&
 #endif /* CONFIG_P2P */
+#ifdef CONFIG_MESH
+	    !(hapd->conf->mesh & MESH_ENABLED) &&
+#endif /* CONFIG_MESH */
 	    os_memcmp(mgmt->bssid, hapd->own_addr, ETH_ALEN) != 0) {
 		wpa_printf(MSG_INFO, "MGMT: BSSID=" MACSTR " not our address",
 			   MAC2STR(mgmt->bssid));
